3c83fa495b1cddeb79904a224e204a7f1827f2a1
werckerbot <pleasemailus@wercker.com>
2016-07-23 12:11:23 +0000

    [ci skip] deploy from mm-git

--- js/main.js
+++ js/main.js
@@ -3,15 +3,7 @@ $(document).ready(function() {
   $('body').removeClass('no-js');
 
   $('a.blog-button').click(function() {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-      $('.content-wrapper').addClass('animated slideInRight');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
-    }
+    $('.panel-cover').addClass('panel-cover--collapsed');
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
@@ -23,7 +15,7 @@ $(document).ready(function() {
   }
 
   $('.btn-mobile-menu').click(function() {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('hidden');
     $('.btn-mobile-close__icon').toggleClass('hidden');
   });
